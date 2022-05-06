80e24373dd5d8a572d49fc66025b1484ea099fea
jgy2808 <jgy2808@naver.com>
2022-05-06 22:47:40 +0900

    Complete demo board_list&board_detail

--- ddo_shop/src/main/java/com/team_stupid/mapper/CustomerMapper.java
+++ ddo_shop/src/main/java/com/team_stupid/mapper/CustomerMapper.java
@@ -1,10 +1,13 @@
 package com.team_stupid.mapper;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Service;
+
+import com.team_stupid.domain.BoardVO;
 import com.team_stupid.security.CustomUserDetails;
 
 @Service
@@ -26,14 +29,16 @@ public interface CustomerMapper {
 	String selectUserIdForFoundid(@Param("username") String username, @Param("email") String email);
 
 	int selectUserPw(@Param("username") String username, @Param("userid") String userid, @Param("email") String email);
-	int getCount();
-
-	void write(@Param("data") Map<String, String> data);
 
 	void updateTempUserPw(@Param("userid") String userid, @Param("tempPw") String tempPw);
 	
-	String search_image(@Param("recycle_title") String recycle_title);
+//	String search_image(@Param("recycle_title") String recycle_title);
 	
-	void deleteboard(@Param("board_num") int board_num);
+	void deleteboard(@Param("event_num") int board_num);
+	List<BoardVO> list();
 
+	BoardVO searchItem(@Param("num") String num);
+	int getCount();
+
+	void write(@Param("data") Map<String, String> data);
 }
