fae2ec6a506c452f1e77db83fdb1b7f1eebf8303
wbwbdya <jh08076@naver.com>
2022-05-11 15:56:53 +0900

    Upload Front File

---
+++ front/assets/js/Subscribe-window.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $("#hide").click(function(){
+        $(".popup").fadeOut();
+    });
+    $("#show").click(function(){
+        $(".popup").fadeIn();
+    });
+     $("#show").click(function(){
+        $("#show").fadeOut();
+    });
+     $("#hide").click(function(){
+        $("#show").fadeIn();
+    });
+});
