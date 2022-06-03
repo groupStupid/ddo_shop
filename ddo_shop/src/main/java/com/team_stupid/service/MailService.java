1967508c75d78af8d6b8f156cfd9f509328b1506
jgy2808 <jgy2808@naver.com>
2022-06-04 04:05:03 +0900

    Update MailService.java

--- ddo_shop/src/main/java/com/team_stupid/service/MailService.java
+++ ddo_shop/src/main/java/com/team_stupid/service/MailService.java
@@ -4,4 +4,5 @@ import com.team_stupid.domain.AccountVO;
 
 public interface MailService {	
 	public void sendMail(AccountVO accountVO) throws Exception;
+	public void sendMail(String verificationCode, String email) throws Exception;
 }
