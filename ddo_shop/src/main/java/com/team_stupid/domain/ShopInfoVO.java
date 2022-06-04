b9a7af7eeeac4f48ec6a5901e4983fee7a1b8f69
jgy2808 <jgy2808@naver.com>
2022-06-05 00:25:35 +0900

    Create and add getter/setter ShopInfoVO

---
+++ ddo_shop/src/main/java/com/team_stupid/domain/ShopInfoVO.java
@@ -0,0 +1,66 @@
+package com.team_stupid.domain;
+
+import java.util.Date;
+
+public class ShopInfoVO {
+	
+	private String SHOP_SERIALNUM;
+	private String SHOP_NAME;
+	private String SHOP_LOCATION;
+	private String SHOP_OPERTIME;
+	private String SHOP_HOLIDAY;
+	private String SHOP_CALL;
+	private String SHOP_NOTICE;
+	private String SHOP_IMAGE;
+	
+	public String getSHOP_SERIALNUM() {
+		return SHOP_SERIALNUM;
+	}
+	public void setSHOP_SERIALNUM(String sHOP_SERIALNUM) {
+		SHOP_SERIALNUM = sHOP_SERIALNUM;
+	}
+	public String getSHOP_NAME() {
+		return SHOP_NAME;
+	}
+	public void setSHOP_NAME(String sHOP_NAME) {
+		SHOP_NAME = sHOP_NAME;
+	}
+	public String getSHOP_LOCATION() {
+		return SHOP_LOCATION;
+	}
+	public void setSHOP_LOCATION(String sHOP_LOCATION) {
+		SHOP_LOCATION = sHOP_LOCATION;
+	}
+	public String getSHOP_OPERTIME() {
+		return SHOP_OPERTIME;
+	}
+	public void setSHOP_OPERTIME(String sHOP_OPERTIME) {
+		SHOP_OPERTIME = sHOP_OPERTIME;
+	}
+	public String getSHOP_HOLIDAY() {
+		return SHOP_HOLIDAY;
+	}
+	public void setSHOP_HOLIDAY(String sHOP_HOLIDAY) {
+		SHOP_HOLIDAY = sHOP_HOLIDAY;
+	}
+	public String getSHOP_CALL() {
+		return SHOP_CALL;
+	}
+	public void setSHOP_CALL(String sHOP_CALL) {
+		SHOP_CALL = sHOP_CALL;
+	}
+	public String getSHOP_NOTICE() {
+		return SHOP_NOTICE;
+	}
+	public void setSHOP_NOTICE(String sHOP_NOTICE) {
+		SHOP_NOTICE = sHOP_NOTICE;
+	}
+	public String getSHOP_IMAGE() {
+		return SHOP_IMAGE;
+	}
+	public void setSHOP_IMAGE(String sHOP_IMAGE) {
+		SHOP_IMAGE = sHOP_IMAGE;
+	}
+	
+	
+}
