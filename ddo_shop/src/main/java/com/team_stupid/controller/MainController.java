28b3ea076074a2ec049a5ec65e8b88419c5eadb4
jgy2808 <jgy2808@naver.com>
2022-06-05 00:23:47 +0900

    Fixed MainController update getEventList

--- ddo_shop/src/main/java/com/team_stupid/controller/MainController.java
+++ ddo_shop/src/main/java/com/team_stupid/controller/MainController.java
@@ -1,13 +1,57 @@
 package com.team_stupid.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.team_stupid.domain.BoardVO;
+import com.team_stupid.mapper.BoardMapper;
 
 @Controller
 public class MainController {
 	
+	@Autowired
+	private BoardMapper boardMapper;
+	
 	@RequestMapping("/main")
-	public String main() {
+	public String main(HttpServletRequest req) {
+		HttpSession ses = req.getSession();
+		
+		List<BoardVO> eventList = boardMapper.list();
+		for (BoardVO event : eventList) {
+			System.out.println(
+					event.getEventTitle() + "\n" +
+					event.getEventContents() + "\n" +
+					event.getEventGoods() + "\n" +
+					event.getEventStart() + "\n" +
+					event.getEventEnd() + "\n" +
+					event.getEventOngoing() + "\n" +
+					event.getEventImageurl()
+					);
+			System.out.println("\n-------------");
+		}
+		ses.setAttribute("eventList", eventList);
+		
+//		List<ShopInfoVO> shopInfoList = shopMapper.getShopInfo();
+//		for (ShopInfoVO shopInfoVO : shopInfoList) {
+//			System.out.println(
+//					shopInfoVO.getSHOP_SERIALNUM() + "\n" +
+//					shopInfoVO.getSHOP_NAME() + "\n" +
+//					shopInfoVO.getSHOP_LOCATION() + "\n" +
+//					shopInfoVO.getSHOP_OPERTIME() + "\n" +
+//					shopInfoVO.getSHOP_HOLIDAY() + "\n" +
+//					shopInfoVO.getSHOP_CALL() + "\n" +
+//					shopInfoVO.getSHOP_NOTICE() + "\n" +
+//					shopInfoVO.getSHOP_IMAGE()
+//					);
+//			System.out.println("\n-------------");
+//		}
+//		ses.setAttribute("shopInfoList", shopInfoList);
 		return "main/main";
 	}
 }
